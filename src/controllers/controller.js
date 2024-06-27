abf3080fad00d81bb6754deaf0a4815edab3f416
Felix Polo <brunolok@gmail.com>
2024-06-27 15:56:50 -0300

    se finalizaron 3 tramas las de mostrar producto por id, actualizar por id y el de añadir productos

--- src/controllers/controller.js
+++ src/controllers/controller.js
@@ -3,8 +3,38 @@ const db = require('../db/db');
 const mostraProductos = (req, res)=>{
     res.json(db);
 }
+const mostarPorId = (req,res)=>{
+    const id = parseInt(req.params.id);
+    const searchProduct = db.find((product)=> product.id === id);
+    res.json(searchProduct);
+}
+const AñadirProduct = (req,res)=>{
+    const {name,quantity,price} = req.body
+    const newProduct = db.push(
+        {
+        id: Math.floor(Math.random() * 100) + 1,
+        name: name,
+        quantity: quantity,
+        price: price 
+        })
+        console.log(newProduct.id);      
+    res.json(newProduct)
+}
+const actualizarProcut = (req, res)=>{
+    const id = parseInt(req.params.id);
+    const {name, price, quantity, } = req.body;
 
+    const searchProduct = db.find((product)=> product.id === id);
+    
+    searchProduct.name = name;
+    searchProduct.price = price.toFixed(2) ;
+    searchProduct.quantity = quantity;
+    res.json(searchProduct);
+}
 
 module.exports={
-    mostraProductos
+    mostraProductos,
+    mostarPorId,
+    AñadirProduct,
+    actualizarProcut
 }
--- src/routers/router.js
+++ src/routers/router.js
@@ -1,6 +1,9 @@
 const router = require('express').Router();
-const {mostraProductos} = require('../controllers/controller');
+const {mostraProductos, mostarPorId, AñadirProduct, actualizarProcut} = require('../controllers/controller');
 
 router.get('/Product',mostraProductos);
+router.get('/Product/:id', mostarPorId);
+router.post('/Product',AñadirProduct);
+router.put('/Product/:id',actualizarProcut)
 
 module.exports = router;
